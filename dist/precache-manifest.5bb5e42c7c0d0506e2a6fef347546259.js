79c530125aef5c3164597ff60f23bbb4a3bb98c5
Kevonia <Tomlinson>
2020-10-09 00:49:03 -0500

    dev

---
+++ dist/precache-manifest.5bb5e42c7c0d0506e2a6fef347546259.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = [
+  {
+    "revision": "7f2c0046c274c463fb05",
+    "url": "/js/chunk-76ea9e48.5be9486b.js"
+  },
+  {
+    "revision": "873f701a30463ac00c45",
+    "url": "/css/app.4a75218a.css"
+  },
+  {
+    "revision": "cec71b5a5cd660021af3",
+    "url": "/css/buefy.80115c7f.css"
+  },
+  {
+    "revision": "cec71b5a5cd660021af3",
+    "url": "/js/buefy.e6a0535e.js"
+  },
+  {
+    "revision": "5b41c08f9852d7be0dd1",
+    "url": "/css/chunk-056b5fb2.9b328847.css"
+  },
+  {
+    "revision": "5b41c08f9852d7be0dd1",
+    "url": "/js/chunk-056b5fb2.f4f54292.js"
+  },
+  {
+    "revision": "d04c380659d82b18286c",
+    "url": "/js/chunk-2d0c49f5.9aeb58bd.js"
+  },
+  {
+    "revision": "fb41a756b9b55aa2e472",
+    "url": "/css/chunk-2fc9faab.3f0dd20e.css"
+  },
+  {
+    "revision": "fb41a756b9b55aa2e472",
+    "url": "/js/chunk-2fc9faab.2d6aa639.js"
+  },
+  {
+    "revision": "bdb184123994389e15e7",
+    "url": "/css/chunk-3f12eb2e.9b328847.css"
+  },
+  {
+    "revision": "bdb184123994389e15e7",
+    "url": "/js/chunk-3f12eb2e.666567b2.js"
+  },
+  {
+    "revision": "fc3a28d94f4c6ba636d8",
+    "url": "/css/chunk-42293f8b.c1522da8.css"
+  },
+  {
+    "revision": "fc3a28d94f4c6ba636d8",
+    "url": "/js/chunk-42293f8b.f0416186.js"
+  },
+  {
+    "revision": "7f2c0046c274c463fb05",
+    "url": "/css/chunk-76ea9e48.0e433876.css"
+  },
+  {
+    "revision": "873f701a30463ac00c45",
+    "url": "/js/app.0dc587bb.js"
+  },
+  {
+    "revision": "934980c5d4e2ba7953c6",
+    "url": "/js/runtime.2cf78dd3.js"
+  },
+  {
+    "revision": "4ed128bcd6be7bdd3d98",
+    "url": "/js/vendor.ec45cac1.js"
+  },
+  {
+    "revision": "498dfb77d0f3b35045a669c01e92dc0f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "02a957a7b9d272fc9aa5de5c1ff6ed07",
+    "url": "/map.png"
+  },
+  {
+    "revision": "f5a9aca12f91c12f22c0ad3258e685d8",
+    "url": "/order.png"
+  },
+  {
+    "revision": "c2ae524227ef9d191a3bb0b25cd46ff0",
+    "url": "/logo.jpeg"
+  },
+  {
+    "revision": "55fa48aae63dbbdc52d80af03309c656",
+    "url": "/cutlery.png"
+  },
+  {
+    "revision": "4dfa7cbd9f23293a39e4c270728c4b1b",
+    "url": "/like.png"
+  },
+  {
+    "revision": "aaeeb91f9c4922c063d66401669c1d81",
+    "url": "/choose.png"
+  },
+  {
+    "revision": "6e9428fad99d2f094f985d01753125ac",
+    "url": "/cargo-truck.png"
+  },
+  {
+    "revision": "b0e68da45daef7326e6e2360981fbb43",
+    "url": "/beer.png"
+  },
+  {
+    "revision": "c11c5c9d3fb8e07da04c2ad551ebac34",
+    "url": "/background-home.jpg"
+  },
+  {
+    "revision": "8b804a4f0f106369f8bf4c07bc8469cd",
+    "url": "/2664646.jpg"
+  }
+];
